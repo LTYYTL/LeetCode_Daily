9ee0b16a08afe2d70102fbb8073063fbe1526775
LTYYTL <lzy991362541@163.com>
2020-12-27 14:18:38 +0800

    Update:205

---
+++ src/com/leetcode/L0205/IsomorphicStringsTest.java
@@ -0,0 +1,24 @@
+package com.leetcode.L0205;
+
+public class IsomorphicStringsTest {
+    public static void main(String[] args) {
+        IsomorphicStrings isomorphicStrings = new IsomorphicStrings();
+        /*** 示例 1:
+         * 输入: s = "egg", t = "add"
+         * 输出: true
+         */
+        System.out.println(isomorphicStrings.isIsomorphic("egg","add"));
+        /**
+         * 示例 2:
+         * 输入: s = "foo", t = "bar"
+         * 输出: false
+         */
+        System.out.println(isomorphicStrings.isIsomorphic("foo","bar"));
+        /**
+         * 示例 3:
+         * 输入: s = "paper", t = "title"
+         * 输出: true
+         */
+        System.out.println(isomorphicStrings.isIsomorphic("paper","title"));
+    }
+}
