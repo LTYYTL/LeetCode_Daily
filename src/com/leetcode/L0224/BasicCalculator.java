fe2310b9918185ce405c7e907759186813296f33
LTYYTL <lzy991362541@163.com>
2021-03-10 11:38:13 +0800

    Update:224

---
+++ src/com/leetcode/L0224/BasicCalculator.java
@@ -0,0 +1,63 @@
+package com.leetcode.L0224;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * 224. 基本计算器
+ * 实现一个基本的计算器来计算一个简单的字符串表达式 s 的值。
+ *
+ * 示例 1：
+ * 输入：s = "1 + 1"
+ * 输出：2
+ *
+ * 示例 2：
+ * 输入：s = " 2-1 + 2 "
+ * 输出：3
+ *
+ * 示例 3：
+ * 输入：s = "(1+(4+5+2)-3)+(6+8)"
+ * 输出：23
+ *
+ * 提示：
+ * 1 <= s.length <= 3 * 105
+ * s 由数字、'+'、'-'、'('、')'、和 ' ' 组成
+ * s 表示一个有效的表达式
+ */
+public class BasicCalculator {
+    public int calculate(String s) {
+        Deque<Integer> ops = new LinkedList<Integer>();
+        ops.push(1);
+        int sign = 1;
+
+        int ret = 0;
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            if (s.charAt(i) == ' ') {
+                i++;
+            } else if (s.charAt(i) == '+') {
+                sign = ops.peek();
+                i++;
+            } else if (s.charAt(i) == '-') {
+                sign = -ops.peek();
+                i++;
+            } else if (s.charAt(i) == '(') {
+                ops.push(sign);
+                i++;
+            } else if (s.charAt(i) == ')') {
+                ops.pop();
+                i++;
+            } else {
+                long num = 0;
+                while (i < n && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + s.charAt(i) - '0';
+                    i++;
+                }
+                ret += sign * num;
+            }
+        }
+        return ret;
+    }
+}
